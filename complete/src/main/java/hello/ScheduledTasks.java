25a113dd45d6304114091d05081bcf48ac85f19c
container1616 <container1616@gmail.com>
2016-03-14 22:52:55 +0000

    added rest template call
    https://spring.io/guides/gs/consuming-rest/

--- complete/src/main/java/hello/ScheduledTasks.java
+++ complete/src/main/java/hello/ScheduledTasks.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ScheduledTasks {
@@ -13,6 +16,9 @@ public class ScheduledTasks {
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        System.out.println("The time is now " + dateFormat.format(new Date()));
+        //System.out.println("The time is now " + dateFormat.format(new Date()));
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
     }
 }
